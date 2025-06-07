c3ad8030fd35ca2f3caeddcd6dda4c77417b964d
Sergey3797 <anufrienko3797@yandex.ru>
2025-06-07 13:46:05 +0300

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -1,70 +1,42 @@
-#Задание №1 c помощью цикла while
-N = int(input("Введите количество чисел"))
-cnt = 0
-i = 0
-while i < N:
-  number = int(input("Введите числа"))
-  if number == 0:
-    cnt += 1
-  i += 1
-print(cnt)    
+# задание 1
+N = int(input("Введите количество чисел: "))
+count_zero = 0
+
+for i in range(N):
+  num = int(input(f"Введите число {i + 1}: "))
+  if num == 0:
+    count_zero += 1
+
+print(f"Количество чисел, равных нулю: {count_zero}")
 
 
+# задание 2
 
-# Задание №1 с помощью цикла for
-N = int(input("Введите количество целых чисел:"))
-cnt = 0
+X = int(input("Введите натуральное число X: "))
+count = 0
+sqrt_X = int(X ** 0.5)  
 
-for i in range(1,N+1):
-  i = int(input())
-  if i == 0:
-    cnt += 1
-print(cnt)
-  
-
-
-# Задание №2 c помощью цикла while
-X = int(input("Введите натуральное число X"))
-cnt_divisor = 0
-i = 1
-
-while i <= X:
+for i in range(1, sqrt_X + 1):
   if X % i == 0:
-    cnt_divisor += 1 
-  i += 1
+    if i * i == X:  
+      count += 1
+    else:
+      count += 2  
 
-print("Количество делителей числа X:",cnt_divisor)  
-
-
-
-# Задание № 2 с помощью цикла for 
-X = int(input("Введите натуральное чсило X"))
-cnt_divisor = 0 
-
-for i in range(1,X+1):
-  if X % i == 0:
-    cnt_divisor += 1
-print("Количество делителей числа X",cnt_divisor)
+print(f"Количество натуральных делителей числа {X}: {count}")
 
 
+# задание 3
 
-#Задание №3 с помощью цикла while
+A = int(input("Введите число A: "))
+B = int(input("Введите число B: "))
 
-A = int(input())
-B = int(input())
+first = True  
+for num in range(A, B + 1):
+  if num % 2 == 0:
+    if not first:
+      print(" ", end="")  
+    print(num, end="")
+    first = False  
 
-if A % 2 != 0: 
-  A += 1
-while A <= B:
-    print(A)
-    A += 2
-
-
-
-#Задание №3 с помощью цикла for
-A = int(input())
-B = int(input())
-
-for i in range(A,B+1):
-  if i % 2 == 0:
-    print(i)    
+print()  
